5b74e2aa06c62f98c294a1b23a413e6b3c0c6f14
ee7klt <kltah1@gmail.com>
2014-12-07 14:57:52 +0800

    views

--- express-app/app.js
+++ express-app/app.js
@@ -8,16 +8,34 @@ var express = require('express');
 //Create an instance of Express
 var app = express();
 
+
+
+//set the view engine
+app.set('view engine', 'jade');
+//where to find the view files
+app.set('views', './views');
+
+//A route for the home page
+
+app.get('/', function (req,res) {
+	res.render('index');
+});
+
+
+app.get('/say-hello', function(req,res) {
+	res.render('hello');
+});
+
+app.get('/test', function(req,res) {
+	res.send('this is a test');
+})
+
 //Start the app
 http.createServer(app).listen(3000, function() {
 	console.log('Express app started');
 })
 
 
-//A route for the home page
 
-app.get('/', function (req,res) {
-	res.send('<h1>Welcome! :)</h1>');
-});
 
 
