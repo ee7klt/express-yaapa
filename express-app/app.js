afb258ef459188029b9c3a3dda7291698f7ec9e2
ee7klt <kltah1@gmail.com>
2015-01-21 12:08:49 +0800

    modularity with routes with node modules - moved route definitions to handler

--- express-app/app.js
+++ express-app/app.js
@@ -1,84 +1,13 @@
-// Include the Node HTTP library
-
-var http = require ('http');
-
-// Include the Express module
 var express = require('express');
-
-//Create an instance of Express
+var http = require('http');
 var app = express();
 
-//logger
-app.use(express.logger({
-	format: ':remote-addr :method :url'
-}));
-
-
-//Load the iniparser module
-var iniparser = require('iniparser');
-//Read the ini file and populate the content on the config object
-var config = iniparser.parseSync('./config.ini');
-
-
-//set the view engine
-app.set('view engine', 'jade');
-//where to find the view files
-app.set('views', './views');
-
-//Mark the public dir as a static dir
-app.use(express.static('./public'));
-
-
-//Add the responseTime middleware
-app.use(express.responseTime());
-
-
-//explicitly add router middleware
+// Add router middleware explicitly
 app.use(app.router);
 
-//Add the errorHandler middleware
-app.use(express.errorHandler());
+//Pass the express instance to the routes module
+var routes = require('./routes')(app);
 
-
-
-
-//Setup for production
-if ('production' == app.get('env')) {
-	app.get('/',function(req,res) {
-		res.render('index',{title:config.title,message:config.message});
-	})
-}
-
-
-//setup for development
-if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
-	app.get('/', function(req,res) {
-		res.send('development mode test');
-	})
-}
-
-
-//common env setup
-app.get('/test', function(req,res) {
-	res.send('works on all environment');
-})
-//A route for the home page
-
-// app.get('/', function (req,res) {
-//   res.render('index', {title:config.title,message:config.message});
-// });
-
-
-app.get('/say-hello', function(req,res) {
-	res.render('hello');
+http.createServer(app).listen(3000, function () {
+	console.log('App started: routes with node modules');
 });
-
-// app.get('/test', function(req,res) {
-// 	res.send('this is a test');
-// })
-
-//Start the app
-http.createServer(app).listen(config.port, function() {
-	console.log('Express app started on port '+config.port);
-})
